f18a3e702d6cb0966b394de1d9afd11611373c43
ifuji <ifujihaku1216@gmail.com>
2026-05-29 14:06:14 +0900

    Componentsの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import { Icon } from "@/components/icon/icon";
+import TextType from "@/components/textType";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-black">
       <div className="flex items-center justify-center">
-        <div className="text-2xl font-bold flex items-center justify-center gap-4 py-4">
+        <div className="text-2xl font-bold flex items-center justify-center gap-4 py-4 text-white">
           <Icon name="code" size={36} />
-          PORTFOLIO
+          <TextType
+            text={["PORTFOLIO"]}
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor
+            cursorCharacter="_"
+            deletingSpeed={50}
+            cursorBlinkDuration={0.5}
+          />
         </div>
       </div>
     </div>
