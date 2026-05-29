b3354e4d8cc6ec7627873edc3875534582d8c153
ifuji <ifujihaku1216@gmail.com>
2026-05-29 14:15:35 +0900

    test 更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="min-h-screen bg-black">
       <div className="flex items-center justify-center">
-        <div className="text-2xl font-bold flex items-center justify-center gap-4 py-4 text-white">
+        <div className="text-2xl font-bold flex items-center justify-center gap-4 py-4 text-white animate-spin">
           <Icon name="code" size={36} />
           <TextType
             text={["PORTFOLIO"]}
